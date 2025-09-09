0e73fc75005a023bca261acd42fceed981733773
jennaf-afk <jennaf@vandenrobotics.com>
2025-09-09 16:32:28 -0700

    Update ExampleCommand.java

--- Rodney_and_jenna/src/main/java/frc/robot/commands/ExampleCommand.java
+++ Rodney_and_jenna/src/main/java/frc/robot/commands/ExampleCommand.java
@@ -2,7 +2,7 @@
 // Open Source Software; you can modify and/or share it under the terms of 
 // the WPILib BSD license file in the root directory of this project. 
 
-package frc.robot.commands;//this is definitely the error not just a fake error to think you found it
+package frc.robot.commands;
 
 import frc.robot.subsystems.ExampleSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
