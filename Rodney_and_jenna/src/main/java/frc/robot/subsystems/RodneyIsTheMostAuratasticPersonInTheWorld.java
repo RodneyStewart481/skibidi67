3f96ae20c15cb4a676b9ebe5a96f3cf0f7cb5eac
RodneyStewart III <rodneys@vandenrobotics.com>
2025-09-16 16:41:36 -0700

    spin motor
    made motor spin and stuff

--- Rodney_and_jenna/src/main/java/frc/robot/subsystems/RodneyIsTheMostAuratasticPersonInTheWorld.java
+++ Rodney_and_jenna/src/main/java/frc/robot/subsystems/RodneyIsTheMostAuratasticPersonInTheWorld.java
@@ -4,16 +4,34 @@
 
 package frc.robot.subsystems;
 
+import com.ctre.phoenix6.hardware.TalonFX;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class RodneyIsTheMostAuratasticPersonInTheWorld extends SubsystemBase {
+  private TalonFX Rodneymotor;
+ 
   /** Creates a new RodneyIsTheMostAuratasticPersonInTheWorld. */
-  public RodneyIsTheMostAuratasticPersonInTheWorld() {}
+ 
+  public RodneyIsTheMostAuratasticPersonInTheWorld() {
+    Rodneymotor = new TalonFX(0);
+     
+  }
+ 
+  public void spinRodneymotor() {
+    Rodneymotor.setVoltage(4);
+  }
 
+  public void stopRodneymotor() {
+    Rodneymotor.setVoltage(0);
+  }
+
+  public void moveRodneymotor(double speed) {
+    Rodneymotor.setVoltage(speed * 12); 
+  }
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
   }
-} public RodneyIsTheMostAuratasticPersonInTheWorld() {
-  
 }
+  
