40ffff17ef7b8d0620f4f09107ca43eb09dd5e05
RodneyStewart III <rodneys@vandenrobotics.com>
2025-09-22 16:49:39 -0700

    InstantCommands
    called upon objects

---
+++ Rodney_and_jenna/src/main/java/frc/robot/commands/InstantCommands1.java
@@ -0,0 +1,32 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj.motorcontrol.Talon;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import frc.robot.subsystems.RodneyIsTheMostAuratasticPersonInTheWorld;
+
+import javax.print.attribute.standard.ReferenceUriSchemesSupported;
+
+import com.ctre.phoenix6.hardware.TalonFX;
+// NOTE:  Consider using this command inline, rather than writing a subclass.  For more
+// information, see:
+// https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
+public class InstantCommands1 extends InstantCommand {
+
+
+  private RodneyIsTheMostAuratasticPersonInTheWorld Rodneysubsystem;
+
+  public InstantCommands1() {
+  
+    // Use addRequirements() here to declare subsystem dependencies.
+    Rodneysubsystem = new RodneyIsTheMostAuratasticPersonInTheWorld();
+  }
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    Rodneysubsystem.stopRodneymotor();
+  }
+}
---
+++ Rodney_and_jenna/src/main/java/frc/robot/commands/InstantCommands2.java
@@ -0,0 +1,28 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import static edu.wpi.first.units.Units.Volt;
+
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+
+import frc.robot.subsystems.RodneyIsTheMostAuratasticPersonInTheWorld;
+// NOTE:  Consider using this command inline, rather than writing a subclass.  For more
+// information, see:
+// https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
+public class InstantCommands2 extends InstantCommand {
+  private RodneyIsTheMostAuratasticPersonInTheWorld Rodneysubsystem;
+  
+  public InstantCommands2() {
+    // Use addRequirements() here to declare subsystem dependencies.
+Rodneysubsystem = new RodneyIsTheMostAuratasticPersonInTheWorld();
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    Rodneysubsystem.spinRodneymotor();
+  }
+}
